cec23084994ac2d097df272419c3fd778376ee30
Zack <spicer.zack8@gmail.com>
2017-01-21 16:49:57 -0500

    Added ability to recieve MQTT messages from topic

--- RRP-Admin/MQTTSubscribeViewController.swift
+++ RRP-Admin/MQTTSubscribeViewController.swift
@@ -11,41 +11,93 @@ import CocoaMQTT
 
 private let PRODUCTION_HOST_NAME = "broker.racereplay.co"
 private let DEV_HOST_NAME = ""
-private let PORT = 1883
+private let PORT: UInt16 = 1883
 
 
 class MQTTSubscribeViewController: UIViewController {
-
+    
+    let alert = UIAlertController(title: "Add new topic", message: "Enter a new topic to add to the list.", preferredStyle: .alert)
+    let defaults = UserDefaults.standard
+    
+    var topics: [String] {
+        get {
+            return UserDefaults.standard.stringArray(forKey: "SavedStringArray") ?? [String]()
+        }
+        set (array) {
+            UserDefaults.standard.set(array, forKey: "SavedStringArray")
+        }
+    }
+    
+    var isSubscribed = false
     var mqtt: CocoaMQTT!
+    
+    @IBOutlet weak var topicTextField: UITextField!
+    @IBOutlet weak var messagesTextView: UITextView!
+    @IBOutlet weak var subscribeButton: UIButton!
+    @IBOutlet weak var topicListView: UIView!
+    @IBOutlet weak var topicPickerView: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mqtt = CocoaMQTT(clientId: "CocoaMQTT-" + String(ProcessInfo().processIdentifier, host: PRODUCTION_HOST_NAME, port: UInt16(PORT)))
+        alert.addTextField { (textField) in
+            textField.text = ""
+        }
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (alertAction) in
+            let textField = self.alert.textFields![0]
+            self.topics.append(textField.text!)
+            self.topicPickerView.reloadAllComponents()
+            textField.text = ""
+        }))
+        
+        topicListView.isHidden = true
+        
+        mqtt = CocoaMQTT(clientId: "CocoaMQTT-" + String(ProcessInfo().processIdentifier), host: PRODUCTION_HOST_NAME, port: PORT)
+        mqtt.keepAlive = 90
+        mqtt.delegate = self
+        mqtt.connect()
+    }
+    
+    @IBAction func subscribeButtonTapped(_ sender: UIButton) {
+        if isSubscribed == false {
+            mqtt.subscribe(topicTextField.text!)
+            isSubscribed = true
+            subscribeButton.setTitle("Unsubscribe", for: .normal)
+        } else {
+            mqtt.unsubscribe(topicTextField.text!)
+            isSubscribed = false
+            subscribeButton.setTitle("Subscribe", for: .normal)
+        }
+    }
+    
+    @IBAction func addTopicButtonTapped(_ sender: UIBarButtonItem) {
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
-extension MQTTSubscribeViewController: CocoaMQTTDelegate {
+extension MQTTSubscribeViewController: CocoaMQTTDelegate, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    // MARK: CocoaMQTTDelegate Methods
     
     func mqtt(_ mqtt: CocoaMQTT, didConnect host: String, port: Int) {
         print("Connected to \(host):\(port)")
     }
     
     func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
-        print("")
+        print("Did Connect Ack")
     }
     
     func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
-        print("")
+        print("Did Publish Message")
     }
     
     func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
-        print("")
+        print("Did Publish Ack")
     }
     
     func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
         if let messageString = message.string {
-            //messageTextField.text = messageTextField.text + "\n" + messageString
+            messagesTextView.text = messagesTextView.text + "\n" + messageString
         }
     }
     
@@ -58,15 +110,46 @@ extension MQTTSubscribeViewController: CocoaMQTTDelegate {
     }
     
     func mqttDidPing(_ mqtt: CocoaMQTT) {
-        print("")
+        print("Did Ping")
     }
     
-    func mqttDidReceivePong(_ mqtt: CocoaMQTT){
-        print("")
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        print("Did Recieve Pong")
     }
     
     func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
         print("Disconnected")
     }
+
+    // MARK: UIPickerViewDataSource Methods
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        let topics = defaults.array(forKey: "SavedStringArray")
+        return topics?.count ?? 0
+    }
+    
+    // MARK: UIPickerViewDelegate Methods
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return topics[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if topics.count >= 1 {
+            topicTextField.text = topics[row]
+        }
+        topicListView.isHidden = true
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        topicListView.isHidden = false
+        return false
+    }
+    
+    
 }
 
